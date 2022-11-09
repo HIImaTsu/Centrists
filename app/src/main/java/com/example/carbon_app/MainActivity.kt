a18e917e3d4222ddbabb16cd1977a662cfa81a26
Rakhat <200103319@stu.sdu.edu.kz>
2022-11-09 18:36:18 +0600

    the start

---
+++ app/src/main/java/com/example/carbon_app/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.carbon_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
