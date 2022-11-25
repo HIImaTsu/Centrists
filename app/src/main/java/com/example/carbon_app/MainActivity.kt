1da3e9d4b1a088d18a7aee3d7be1a0c47b483ba9
200103002 <200103002@stu.sdu.edu.kz>
2022-11-25 20:21:32 +0600

    MainActivity.kt/username/password/loginbtn/function

--- app/src/main/java/com/example/carbon_app/MainActivity.kt
+++ app/src/main/java/com/example/carbon_app/MainActivity.kt
@@ -2,7 +2,10 @@ package com.example.carbon_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import android.widget.TextView
+import com.google.android.material.button.MaterialButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,5 +15,10 @@ class MainActivity : AppCompatActivity() {
         val username = (findViewById<TextView>(R.id.username))
         val password = (findViewById<TextView>(R.id.password))
 
+        var loginbtn = (findViewById<MaterialButton>(R.id.loginbtn))
+
+        loginbtn.setOnClickListener(View.OnClickListener {
+//pustota zapolni
+        })
     }
 }
