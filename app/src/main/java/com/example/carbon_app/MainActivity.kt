262a5de5fd2f4f8b0bb2fd48a150c2d07992b54a
200103002 <200103002@stu.sdu.edu.kz>
2022-11-25 20:12:32 +0600

    MainActivity.kt/ username and password

--- app/src/main/java/com/example/carbon_app/MainActivity.kt
+++ app/src/main/java/com/example/carbon_app/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.carbon_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login)
+
+        val username = (findViewById<TextView>(R.id.username))
+        val password = (findViewById<TextView>(R.id.password))
+
     }
 }
