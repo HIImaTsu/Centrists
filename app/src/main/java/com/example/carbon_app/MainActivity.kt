9f456b3d1891bd265519c075e170b1e6ed83e389
Rakhat <200103319@stu.sdu.edu.kz>
2022-11-27 02:20:15 +0600

    /MainActivity edit Toast

--- app/src/main/java/com/example/carbon_app/MainActivity.kt
+++ app/src/main/java/com/example/carbon_app/MainActivity.kt
@@ -12,22 +12,19 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.login)
 
         val login2: Button = findViewById(R.id.loginbtn)
-        login2.setOnClickListener{onClick()}
+        login2.setOnClickListener{
+            onClick()
+        }
 
     }
-
     private fun onClick(){
-
         val username : TextView  = findViewById(R.id.username)
         val password : TextView = findViewById(R.id.password)
 
         if(username.text.toString() == "admin" && password.text.toString() == "admin"){
-            Toast.makeText(applicationContext,"LOGIN SUCCESSFUL", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this,"LOGIN SUCCESSFUL", Toast.LENGTH_SHORT).show()
         }else
-            Toast.makeText(applicationContext,"LOGIN FAILED!!!", Toast.LENGTH_SHORT).show()
-
-
-
+            Toast.makeText(this,"LOGIN FAILED!!!", Toast.LENGTH_SHORT).show()
     }
 
 
