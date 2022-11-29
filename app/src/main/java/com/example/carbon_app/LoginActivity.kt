60919de101d4c53d3a27c45379dd3fed4d975884
Rakhat <200103319@stu.sdu.edu.kz>
2022-11-29 19:11:13 +0600

    add class

---
+++ app/src/main/java/com/example/carbon_app/LoginActivity.kt
@@ -0,0 +1,32 @@
+package com.example.carbon_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login)
+
+        val login2: Button = findViewById(R.id.loginbtn)
+        login2.setOnClickListener {
+            onClick()
+        }
+    }
+
+
+            private fun onClick(){
+        val username : TextView = findViewById(R.id.username)
+        val password : TextView = findViewById(R.id.password)
+
+        if(username.text.toString() == "admin" && password.text.toString() == "admin"){
+            Toast.makeText(this,"LOGIN SUCCESSFUL", Toast.LENGTH_SHORT).show()
+        }else
+            Toast.makeText(this,"LOGIN FAILED!!!", Toast.LENGTH_SHORT).show()
+    }
+
+
+    }
--- app/src/main/java/com/example/carbon_app/MainActivity.kt
+++ app/src/main/java/com/example/carbon_app/MainActivity.kt
@@ -15,6 +15,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var button : Button
     private lateinit var button2 : Button
     private val mainFragment = MainFragment()
+    private val secondFragment = SecondFragment()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,9 +27,7 @@ class MainActivity : AppCompatActivity() {
         button.setOnClickListener{
                 setNewFragment(mainFragment)
         }
-
         button2.setOnClickListener{
-             val secondFragment = SecondFragment()
                 setNewFragment(secondFragment)
         }
 
@@ -47,19 +46,3 @@ class MainActivity : AppCompatActivity() {
 
 
 
-//    private fun onClick(){
-//
-//
-//        val username : TextView  = findViewById(R.id.username)
-//        val password : TextView = findViewById(R.id.password)
-//
-//        if(username.text.toString() == "admin" && password.text.toString() == "admin"){
-//            Toast.makeText(this,"LOGIN SUCCESSFUL", Toast.LENGTH_SHORT).show()
-//        }else
-//            Toast.makeText(this,"LOGIN FAILED!!!", Toast.LENGTH_SHORT).show()
-//    }
-
-//        val login2: Button = findViewById(R.id.loginbtn)
-//        login2.setOnClickListener{
-//            onClick()
-//        }
