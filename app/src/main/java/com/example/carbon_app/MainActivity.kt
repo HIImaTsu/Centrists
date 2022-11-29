8d7f91d7f6c0152c65288bd78249f9bffbf3abdb
200103002 <200103002@stu.sdu.edu.kz>
2022-11-29 21:07:12 +0600

    private VPAdapter

--- app/src/main/java/com/example/carbon_app/MainActivity.kt
+++ app/src/main/java/com/example/carbon_app/MainActivity.kt
@@ -8,41 +8,13 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
+import androidx.viewpager.widget.ViewPager
 import com.example.carbon_app.databinding.FragmentMainBinding
+import com.google.android.material.tabs.TabItem
+import com.google.android.material.tabs.TabLayout
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var button : Button
-    private lateinit var button2 : Button
-    private val mainFragment = MainFragment()
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        button = findViewById(R.id.btn_frag1)
-        button2 = findViewById(R.id.btn_frag2)
-
-        button.setOnClickListener{
-                setNewFragment(mainFragment)
-        }
-
-        button2.setOnClickListener{
-             val secondFragment = SecondFragment()
-                setNewFragment(secondFragment)
-        }
-
-    }
-
-    private fun setNewFragment(fragment: Fragment) {
-        val manager = supportFragmentManager.beginTransaction()
-        manager.replace(R.id.frame_layout, fragment)
-        manager.addToBackStack(null)
-        manager.commit()
-    }
-
-
-}
 
 
 
--- app/src/main/java/com/example/carbon_app/VPAdapter.kt
+++ app/src/main/java/com/example/carbon_app/VPAdapter.kt
@@ -1,4 +1,11 @@
 package com.example.carbon_app
 
-class VPAdapter {
+import android.icu.text.CaseMap.Title
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+
+class VPAdapter(fm: FragmentManager, behavior: Int) : FragmentPagerAdapter(fm, behavior) {
+
+
 }
