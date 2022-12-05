863af36547d54d67c0b546ffeddd4d793ac019b0
Rakhat <200103319@stu.sdu.edu.kz>
2022-12-05 20:03:49 +0600

    add viewModel

--- app/src/main/java/com/example/carbon_app/LoginActivity.kt
+++ app/src/main/java/com/example/carbon_app/LoginActivity.kt
@@ -6,11 +6,23 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import androidx.lifecycle.LiveData
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var username: TextView
+    private lateinit var password: TextView
+    private lateinit var currentAttempt: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login)
+
+//        val manager = supportFragmentManager.beginTransaction()
+//        manager.replace(R.id.rev_layout, loginFragment)
+//        manager.addToBackStack(null)
+//        manager.commit()
+
 
         val login2: Button = findViewById(R.id.loginbtn)
         login2.setOnClickListener {
@@ -20,8 +32,10 @@ class LoginActivity : AppCompatActivity() {
 
 
             private fun onClick(){
-        val username : TextView = findViewById(R.id.username)
-        val password : TextView = findViewById(R.id.password)
+                currentAttempt = findViewById(R.id.attempt_left)
+
+                username = findViewById(R.id.username)
+                password = findViewById(R.id.password)
 
         if(username.text.toString() == "admin" && password.text.toString() == "admin"){
             Toast.makeText(this,"LOGIN SUCCESSFUL", Toast.LENGTH_SHORT).show()
@@ -29,8 +43,10 @@ class LoginActivity : AppCompatActivity() {
             startActivity(intent)
 
         }else
+            currentAttempt.text = "2"
             Toast.makeText(this,"LOGIN FAILED!!!", Toast.LENGTH_SHORT).show()
     }
-
-
     }
+
+
+
