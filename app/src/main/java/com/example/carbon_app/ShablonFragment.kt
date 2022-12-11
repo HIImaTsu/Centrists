5b6edcb8ed38684f34102c326f8e4b1aefd5f370
200103002 <200103002@stu.sdu.edu.kz>
2022-12-11 18:50:35 +0600

    fblock

--- app/src/main/java/com/example/carbon_app/ShablonFragment.kt
+++ app/src/main/java/com/example/carbon_app/ShablonFragment.kt
@@ -23,19 +23,19 @@ class ShablonFragment : Fragment() {
         // Inflate the layout for this fragment
 
         val binding = FragmentShablonBinding.inflate(inflater, container, false)
-//        binding.bottomNavigationView.setOnItemSelectedListener {id ->
-//            when (id) {
-//                R.id.home -> setNewFragment(mainFragment)
-//                R.id.faculty -> setNewFragment(secondFragment)
-//                R.id.person -> setNewFragment(thirdFragment)
-//                else -> {R.id.exit -> setNewFragment(mainFragment)}
-//            }
-//        }
-
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.home -> setNewFragment(mainFragment)
+                R.id.faculty -> setNewFragment(secondFragment)
+                R.id.person -> setNewFragment(thirdFragment)
+                else -> {setNewFragment(mainFragment)}
+            }
+            true
+        }
         return binding.root
     }
 
-    fun setNewFragment(fragment: Fragment) {
+    private fun setNewFragment(fragment: Fragment) {
         val manager = childFragmentManager.beginTransaction()
         manager.replace(R.id.frame_layout, fragment)
         manager.addToBackStack(null)
